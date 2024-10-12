ece52a93e944a785f7e07afc3b3e40c7ec2554c4
spiriton <vanya.ershov.05@mail.ru>
2024-10-12 04:24:40 +0300

    some prints

--- task1/main.py
+++ task1/main.py
@@ -1,19 +1,29 @@
 import os
+from copy import deepcopy
 
 
-def simplex_method(C, A, b, eps=0.00001):
-    A = A.copy()
+def simplex_method(C, A, b, f, eps=0.00001):
+    n = len(C)
+    m = len(b)
+    f.write("Max: z = ")
+    f.write(" + ".join([str(C[i]) + f" * x{i + 1}" for i in range(len(C))]).replace("+ -", "- "))
+    f.write("\n\nSubject to the constraints:\n")
+    for i in range(m):
+        f.write(" + ".join([f"{A[i][j]} * x{j + 1}" for j in range(n)]).replace("+ -", "- "))
+        f.write(f" <= {b[i]}\n")
+    f.write("\n")
+
+    A = deepcopy(A)
     flag = False
     z = list(map(lambda x: -float(x), C))
     eps = float(strs[-1])
-    n = len(z)
-    m = len(b)
     bz = 0
     basics = [n + i for i in range(m)]
     for i in range(m):
         for j in range(m):
             A[i].append(1 if i == j else 0)
         z.append(0)
+
     while True:
         mini = min(z)
         if mini >= 0:
@@ -43,7 +53,7 @@ def simplex_method(C, A, b, eps=0.00001):
         for i in range(n + m):
             z[i] -= A[ind2][i] * d
         bz -= b[ind2] * d
-        if abs(b[ind2] * d) < eps:
+        if -b[ind2] * d < eps:
             break
     if flag:
         return False, [], 0
@@ -61,11 +71,13 @@ for file in os.listdir("input"):
         A0 = [list(map(float, s.strip().split())) for s in strs[1:-2]]
         b0 = list(map(float, strs[-2].strip().split()))
         eps0 = float(strs[-1])
-        res = simplex_method(C0, A0, b0, eps0)
     with open(f'output/{file.strip(".txt")}.txt', 'w') as f:
+        res = simplex_method(C0, A0, b0, f, eps0)
         if not res[0]:
             f.write("The method is not applicable!")
         else:
-            f.write(" ".join(map(lambda x: str(round(x, 5)), res[1])))
-            f.write("\n")
+            x = list(map(lambda x: str(round(x, 5)), res[1]))
+            f.write("Solution:\n")
+            f.write(", ".join([f"x{i + 1} = {x[i]}" for i in range(len(x))]))
+            f.write("\nz = ")
             f.write(str(round(res[2], 5)))
